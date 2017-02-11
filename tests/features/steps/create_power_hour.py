00d9da4f57c79372fb23591a6a7d39513bc5e288
James Castiglione <JamesCastiglione241@gmail.com>
2017-02-11 15:21:07 -0500

    Makes test download 2 tracks

--- tests/features/steps/create_power_hour.py
+++ tests/features/steps/create_power_hour.py
@@ -2,8 +2,8 @@ from behave import *
 from PyQt5.QtTest import QTest
 from PyQt5.Qt import Qt, QPoint
 from unittest.mock import Mock
+from hamcrest import *
 import os
-from should_dsl import should
 from power_hour_creator.ui.power_hour_creator_window import ExportPowerHourDialog
 
 use_step_matcher("re")
@@ -18,12 +18,14 @@ def step_impl(context):
     :type context: behave.runner.Context
     """
     tracklist = context.main_window.tracklist
-    url_cell_pos = (tracklist.rowViewportPosition(1), tracklist.columnViewportPosition(1))
     viewport = tracklist.viewport()
-    QTest.mouseClick(viewport, Qt.LeftButton, pos=QPoint(url_cell_pos[0], url_cell_pos[1]))
-    QTest.mouseDClick(viewport, Qt.LeftButton, pos=QPoint(url_cell_pos[0], url_cell_pos[1]))
-    QTest.keyClicks(viewport.focusWidget(), track_url)
-    QTest.keyClick(viewport.focusWidget(), Qt.Key_Return)
+    for track_num in range(2):
+        url_cell_pos = QPoint(tracklist.columnViewportPosition(0),
+                              tracklist.rowViewportPosition(track_num + 1))
+        QTest.mouseClick(viewport, Qt.LeftButton, pos=url_cell_pos)
+        QTest.mouseDClick(viewport, Qt.LeftButton, pos=url_cell_pos)
+        QTest.keyClicks(viewport.focusWidget(), track_url)
+        QTest.keyClick(viewport.focusWidget(), Qt.Key_Return)
 
 
 @step("I create a power hour")
@@ -52,5 +54,5 @@ def step_impl(context):
     while export_dialog_visible():
         QTest.qWait(100)
 
-    assert(os.path.exists(context.export_path))
+    assert_that(os.path.exists(context.export_path), is_(True))
 
